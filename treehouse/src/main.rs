36e555e29cb9514ac65a69b3645625ebbac0c936
Nuno Martins <nmartins10@gmail.com>
2021-10-27 13:05:21 +0100

    treehouse

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,5 +1,45 @@
 use std::io::stdin;
 
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    action: VisitorAction,
+    age: i8,
+}
+
+impl Visitor {
+    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            action,
+            age,
+        }
+    }
+
+    fn greet_visitor(&self) {
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the treehouse, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}", self.name);
+                println!("{}", note);
+                if self.age < 21 {
+                    println!("Do not serve alcohol to {}", self.name);
+                }
+            }
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -9,21 +49,36 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list = ["bert", "steve", "fred"];
+    let mut visitor_list = vec![
+        Visitor::new("Bert", VisitorAction::Accept, 45),
+        Visitor::new(
+            "Steve",
+            VisitorAction::AcceptWithNote {
+                note: String::from("Lactose-free milk is in the fridge"),
+            },
+            15,
+        ),
+        Visitor::new("Fred", VisitorAction::Refuse, 30),
+    ];
 
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
+        let name = what_is_your_name();
 
-    let mut allow_them_in = false;
-    for visitor in visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+                }
+            }
         }
     }
 
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list.");
-    }
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
